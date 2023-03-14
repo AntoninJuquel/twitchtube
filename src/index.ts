337b9f28c2ab33e96224178713e771d62ad24e05
AntoninJuquel <antonin.juquel@outlook.fr>
2023-03-14 08:53:36 +0100

    WIP: interactive CLI
    https://medium.com/skilllane/build-an-interactive-cli-application-with-node-js-commander-inquirer-and-mongoose-76dc76c726b6

--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,80 @@
-import shutdown from "@hypercliq/shutdown-cleanup";
-import dotenv from "dotenv";
-import { hoursToMilliseconds } from "date-fns";
+// import shutdown from "@hypercliq/shutdown-cleanup";
+// import dotenv from "dotenv";
+// import { hoursToMilliseconds } from "date-fns";
 
-import config from "./config";
-import log, { logger } from "./logger";
-import ascii from "./ascii";
+// import config from "./config";
+// import log, { logger } from "./logger";
+// import ascii from "./ascii";
 
-import * as twitch from "./twitch";
-import * as video from "./video-merge";
-import * as youtube from "./youtube";
-import { File } from "winston/lib/winston/transports";
-import fs from "fs";
-import path from "path";
+// import * as twitch from "./twitch";
+// import * as video from "./video-merge";
+// import * as youtube from "./youtube";
+// import { File } from "winston/lib/winston/transports";
+// import fs from "fs";
+// import path from "path";
 
-async function start() {
-  console.log(ascii);
+// async function start() {
+//   console.log(ascii);
 
-  dotenv.config();
-  shutdown.registerHandler(exit);
-  log.init();
+//   dotenv.config();
+//   shutdown.registerHandler(exit);
+//   log.init();
 
-  logger.info("Loaded environment variables");
-  logger.info(`env: ${process.env.NODE_ENV}`);
+//   logger.info("Loaded environment variables");
+//   logger.info(`env: ${process.env.NODE_ENV}`);
 
-  logger.info("Initializing modules");
-  await twitch.init(config, logger);
-  video.init(config, logger);
-  youtube.init(config, logger);
-  logger.info("Started");
-}
+//   logger.info("Initializing modules");
+//   await twitch.init(config, logger);
+//   video.init(config, logger);
+//   youtube.init(config, logger);
+//   logger.info("Started");
+// }
 
-async function main() {
-  await twitch.downloadClips();
-  await video.mergeClips();
-  await youtube.uploadVideo();
-}
+// async function main() {
+//   await twitch.downloadClips();
+//   await video.mergeClips();
+//   await youtube.uploadVideo();
+// }
 
-async function end() {
-  logger.transports.forEach((t) => {
-    if (t instanceof File) {
-      fs.writeFileSync(path.join(t.dirname, t.filename), "");
-    }
-  });
-}
+// async function end() {
+//   logger.transports.forEach((t) => {
+//     if (t instanceof File) {
+//       fs.writeFileSync(path.join(t.dirname, t.filename), "");
+//     }
+//   });
+// }
 
-async function exit() {
-  logger.transports.forEach((t) => {
-    if (t instanceof File) {
-      fs.writeFileSync(path.join(t.dirname, t.filename), "");
-    }
-  });
-}
+// async function exit() {
+//   logger.transports.forEach((t) => {
+//     if (t instanceof File) {
+//       fs.writeFileSync(path.join(t.dirname, t.filename), "");
+//     }
+//   });
+// }
 
-start()
-  .then(async () => {
-    main().then(end);
+// start()
+//   .then(async () => {
+//     main().then(end);
 
-    await new Promise((resolve, reject) =>
-      setInterval(() => {
-        main().then(end).catch(reject);
-      }, hoursToMilliseconds(config.INTERVAL))
-    );
+//     await new Promise((resolve, reject) =>
+//       setInterval(() => {
+//         main().then(end).catch(reject);
+//       }, hoursToMilliseconds(config.INTERVAL))
+//     );
+//   })
+//   .finally(() => exit());
+
+import inquirer from "inquirer";
+
+inquirer
+  .prompt([{ type: "input", name: "name", message: "What's your name?" }])
+  .then((answers) => {
+    console.log(answers);
   })
-  .finally(() => exit());
+  .catch((error) => {
+    if (error.isTtyError) {
+      console.log("Prompt couldn't be rendered in the current environment");
+    } else {
+      console.log(error);
+    }
+  });
